b683c37ff401f1afa200b45c855bf788265bd80d
JDiamond1999 <122535467+JDiamond1999@users.noreply.github.com>
2023-02-23 16:30:02 +0000

    Making the form submission post to homepage

--- index.js
+++ index.js
@@ -48,7 +48,18 @@ app.get("/viewrecord", function (req, res) {
     res.render('viewrecord');
 });
 
-app.post("/signin", (req,res) => {
+// app.post("/signin", (req,res) => {
+//     // printing the form data to console as an array
+//     console.log(req.body);
+
+//     // storing the form data as an object
+//     let userdata = req.body;
+
+//     // send data back to home page as a json object
+//     res.render('index', { sentback : userdata });
+// })
+
+app.post("/register", (req,res) => {
     // printing the form data to console as an array
     console.log(req.body);
 
