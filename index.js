7ae14d5d55adf646d2b15cf9b6368916fdada78b
JDiamond1999 <122535467+JDiamond1999@users.noreply.github.com>
2023-03-13 15:00:20 +0000

    got adding records to work

--- index.js
+++ index.js
@@ -15,42 +15,42 @@ app.set("view engine", "ejs");
 
 //routes
 app.get("/", function (req, res) {
-  res.render("index");
+    res.render("index");
 });
 
 app.get("/signin", function (req, res) {
-  res.render("signin");
+    res.render("signin");
 });
 
 app.get("/register", function (req, res) {
-  res.render("register");
+    res.render("register");
 });
 
 app.get("/addcollection", function (req, res) {
-  res.render("addcollection");
+    res.render("addcollection");
 });
 
 app.get("/addrecord", function (req, res) {
-  let readgenres = `SELECT genre_name, genre_id 
+    let readgenres = `SELECT genre_name, genre_id 
     FROM genre;`;
 
-  connection.query(readgenres, (err, rows) => {
-    if (err) throw err;
-    let rowdata = rows;
-    res.render("addrecord", { rowdata });
-  });
+    connection.query(readgenres, (err, rows) => {
+        if (err) throw err;
+        let rowdata = rows;
+        res.render("addrecord", { rowdata });
+    });
 });
 
 app.post("/add", (req, res) => {
-  let image_link = req.body.imagelink;
-  let title = req.body.title;
-  let artist = req.body.artist;
-  let release_year = req.body.releaseyear;
-  let record_label = req.body.recordlabel;
-  let genre_id = req.body.genre;
-  let lastinsert;
+    let image_link = req.body.imagelink;
+    let title = req.body.title;
+    let artist = req.body.artist;
+    let release_year = req.body.releaseyear;
+    let record_label = req.body.recordlabel;
+    let genre_id = req.body.genre;
+    let lastinsert;
 
-  let insertartistrecord = `INSERT INTO artist
+    let insertartistrecord = `INSERT INTO artist
   (artist_name, artist_desc)
   VALUES (?, "null");
 
@@ -58,48 +58,51 @@ app.post("/add", (req, res) => {
   
   INSERT INTO record
   (record_name, cover_image, year_of_release, record_label, record_likes, genre_id, artist_id )
-  VALUES( ? , ? , ? , ? , 0 , ? , @artistid );`;
+  VALUES( ? , ? , ? , ? , 0 , ? , @artistid );
+  
+  SET @recordid = LAST_INSERT_ID();
+  
+  INSERT INTO record_track (record_track_id, record_id, track_id) VALUES (NULL, @recordid, '73');`;
 
-  connection.query(
-    insertartistrecord,
-    [artist, title, image_link, release_year, record_label, genre_id],
-    (err, rows) => {
-      if (err) throw err;
-      lastinsert = rows.insertId;
-      console.log(lastinsert);
-      res.send(
-        `You have inserted::: <p>${artist}</p> to the artists database, and ${title} into the record table`
-      );
-    }
-  );
+    connection.query(
+        insertartistrecord,
+        [artist, title, image_link, release_year, record_label, genre_id],
+        (err, rows) => {
+            if (err) throw err;
+
+            res.send(
+                `You have inserted::: <p>${artist}</p> to the artists database, and ${title} into the record table`
+            );
+        }
+    );
 });
 
 app.get("/managecollections", function (req, res) {
-  res.render("managecollections");
+    res.render("managecollections");
 });
 
 // trying to load in our data from sql
 app.get("/managerecords", function (req, res) {
-  // chris says dont use select * from as it increases loading time/dont need resources
-  let readrecords = `SELECT * 
+    // chris says dont use select * from as it increases loading time/dont need resources
+    let readrecords = `SELECT * 
     FROM record 
     INNER JOIN artist 
     ON record.artist_id=artist.artist_id;`;
 
-  connection.query(readrecords, (err, rows) => {
-    if (err) throw err;
-    let rowdata = rows;
-    res.render("managerecords", { rowdata });
-  });
+    connection.query(readrecords, (err, rows) => {
+        if (err) throw err;
+        let rowdata = rows;
+        res.render("managerecords", { rowdata });
+    });
 });
 
 app.get("/viewcollection", function (req, res) {
-  res.render("viewcollection");
+    res.render("viewcollection");
 });
 
 app.get("/viewrecord", function (req, res) {
-  let showid = req.query.recordid;
-  let readsql = `SELECT *
+    let showid = req.query.recordid;
+    let readsql = `SELECT *
     FROM track
     INNER JOIN record_track
     ON track.track_id = record_track.track_id
@@ -109,11 +112,12 @@ app.get("/viewrecord", function (req, res) {
     ON record.artist_id = artist.artist_id
     WHERE record.record_id = ?`;
 
-  connection.query(readsql, [showid], (err, rows) => {
-    if (err) throw err;
-    let rowdata = rows;
-    res.render(`viewrecord`, { rowdata });
-  });
+    connection.query(readsql, [showid], (err, rows) => {
+        if (err) throw err;
+        let rowdata = rows;
+        res.render(`viewrecord`, { rowdata });
+        console.log(rows);
+    });
 });
 
 // app.post("/signin", (req,res) => {
@@ -128,32 +132,32 @@ app.get("/viewrecord", function (req, res) {
 // })
 
 app.post("/register", (req, res) => {
-  // printing the form data to console as an array
-  console.log(req.body);
+    // printing the form data to console as an array
+    console.log(req.body);
 
-  // storing the form data as an object
-  let userdata = req.body;
+    // storing the form data as an object
+    let userdata = req.body;
 
-  // send data back to home page as a json object
-  res.render("index", { sentback: userdata });
+    // send data back to home page as a json object
+    res.render("index", { sentback: userdata });
 });
 
 // port 3306 is the port our database is using (just check on xampp to make sure)
 const connection = mysql.createConnection({
-  host: `localhost`,
-  user: `root`,
-  password: ``,
-  database: `stacks_of_wax_1`,
-  port: `3306`,
-  multipleStatements: `true`,
+    host: `localhost`,
+    user: `root`,
+    password: ``,
+    database: `stacks_of_wax_1`,
+    port: `3306`,
+    multipleStatements: `true`,
 });
 
 connection.connect(function (err) {
-  if (err) {
-    return console.error(`error` + err.message);
-  }
+    if (err) {
+        return console.error(`error` + err.message);
+    }
 
-  console.log(`Connected to MySql Server`);
+    console.log(`Connected to MySql Server`);
 });
 
 //server
