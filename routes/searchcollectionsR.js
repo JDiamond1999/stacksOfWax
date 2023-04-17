9e7fb39e76f08353c95a8e17ec14f19a69672962
Jack Diamond <jackdiamond199@gmail.com>
2023-04-17 13:25:54 +0100

    minor tweaks at home on laptop, making search available for all

--- routes/searchcollectionsR.js
+++ routes/searchcollectionsR.js
@@ -2,31 +2,22 @@ const express = require(`express`);
 const searchcollectionsR = express.Router();
 const connection = require(`../middleware/db`);
 
-
 searchcollectionsR.get("/searchcollections", function (req, res) {
+  let sessionobj = req.session;
+  let userid = sessionobj.authen;
 
-    let sessionobj = req.session;
-    let userid = sessionobj.authen;
-
-    if (sessionobj.authen) {
-
-        // chris says dont use select * from as it increases loading time/dont need resources
-        let readcollections =
-            `SELECT * 
+  // chris says dont use select * from as it increases loading time/dont need resources
+  let readcollections = `SELECT * 
             FROM collection
             INNER JOIN user
             ON collection.user_id = user.user_id;`;
 
-        connection.query(readcollections, (err, rows) => {
-            if (err) throw err;
-            let rowdata = rows;
-           
-            res.render("searchcollections", { rowdata });
-        });
+  connection.query(readcollections, (err, rows) => {
+    if (err) throw err;
+    let rowdata = rows;
 
-    } else {
-        res.render('signin');
-    }
+    res.render("searchcollections", { rowdata });
+  });
 });
 
 module.exports = searchcollectionsR;
